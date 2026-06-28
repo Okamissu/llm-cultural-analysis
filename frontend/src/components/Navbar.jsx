c78c66bf7dd5c034d2d541017996bbd50b982f8c
Okamissu <okamissu@gmail.com>
2026-06-28 20:00:39 +0200

    feat(frontend): update Navbar to include logo and improve title display

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,8 +26,12 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 border-b bg-white/80 backdrop-blur">
       <nav className="mx-auto flex max-w-7xl items-center justify-between px-8 py-4">
-        <h1 className="text-xl font-bold tracking-tight">
-          {t('navbar.title')}
+        <h1 className="text-xl font-bold tracking-tight flex gap-2">
+          <img
+            className="hidden sm:inline-block w-7 "
+            src={'../../public/icon.png'}
+          ></img>
+          <span className="hidden lg:inline">{t('navbar.title')}</span>
         </h1>
 
         <div className="flex items-center gap-6">
@@ -37,11 +41,8 @@ export default function Navbar() {
                 key={link.to}
                 to={link.to}
                 className={({ isActive }) =>
-                  `rounded-lg px-4 py-2 text-sm font-medium transition ${
-                    isActive
-                      ? 'bg-slate-900 text-white'
-                      : 'text-slate-600 hover:bg-slate-100'
-                  }`
+                  `rounded-lg px-4 py-2 text-sm font-medium transition whitespace-nowrap
+ ${isActive ? 'bg-slate-900 text-white' : 'text-slate-600 hover:bg-slate-100'}`
                 }
               >
                 {t(link.key)}
