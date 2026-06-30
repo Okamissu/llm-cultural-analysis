d3205608c482f3e26eebf7d98577a3aff3e36fb4
Okamissu <okamissu@gmail.com>
2026-06-30 19:12:06 +0200

    chore(backend): server built frontend via express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import compareRoutes from './routes/compare.js'
 import experimentRoutes from './routes/experiment.js'
@@ -9,23 +11,37 @@ import errorHandler from './middlewares/errorHandler.js'
 
 import { PORT } from './config/config.js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'API works!',
-  })
-})
+app.use(express.static(path.join(__dirname, '../public')))
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'API works!',
+//   })
+// })
 
 app.use('/api/compare', compareRoutes)
 app.use('/api/experiments', experimentRoutes)
 app.use('/api/statistics', statisticsRoutes)
 
+app.use((req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next()
+  }
+
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 app.use(errorHandler)
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
